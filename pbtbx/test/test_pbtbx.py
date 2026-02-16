37855a3f54e653fd406235f66c78fe4a86f1e7de
Peter Briggs <peter.briggs@manchester.ac.uk>
2026-02-16 13:26:34 +0000

    Add trivial test case for pbtbx (so CI workflow has something to run on).

---
+++ pbtbx/test/test_pbtbx.py
@@ -0,0 +1,11 @@
+# Placeholder unit test for pbtbx
+
+from unittest import TestCase
+
+class TestPbtbx(TestCase):
+
+    def test_pbtbx(self):
+        """
+        Trivial test case
+        """
+        pass
